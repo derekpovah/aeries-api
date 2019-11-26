4934c85ad6f336e9ce8bcc2d2a2622e104d7ffd9
Derek Povah <derek@derekpovah.com>
2019-11-26 13:52:59 -0800

    Allow base_uri to be set on the fly or from an environment variable

--- lib/aeries/api/client.rb
+++ lib/aeries/api/client.rb
@@ -12,12 +12,13 @@ module Aeries
       include Aeries::Api::Client::Students
       include Aeries::Api::Client::StudentGroups
 
-      base_uri ENV['AERIES_BASE_URI']
       format :json
 
-      def initialize(aeries_cert = nil)
+      def initialize(aeries_cert: nil, base_uri: nil)
         aeries_cert ||= ENV['AERIES_CERT']
-        self.class.default_options.merge!(headers: { 'AERIES-CERT': aeries_cert })
+        base_uri ||= ENV['AERIES_BASE_URI']
+
+        self.class.default_options.merge!(headers: { 'AERIES-CERT': aeries_cert }, base_uri: base_uri)
       end
     end
   end
